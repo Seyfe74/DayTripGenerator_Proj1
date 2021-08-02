a5cf249133a637115358c4f908dab775b51f62f9
Aferu Minas <seyfeminas@gmail.com>
2021-08-02 15:35:48 -0700

    Project fully developed and ready for submission

--- dTripGenerator.js
+++ dTripGenerator.js
@@ -6,6 +6,7 @@
 //   Description:  This program will assist user to genarate ideal trip plan.
 
 
+//  Array of cities, restaurants, transportation mode, and entertainments to be suggested to user 
 
 let destinations = ["Bangkok", "Paris", "London", "Dubai", "Singapore", "KualaLumpur", "NewYork", "Istanbul", "Tokyo", "Antalya", "Seoul", "Osaka", "Makkah", "Phuket", "Pattaya", "Milan", "Barcelona", "PalmaDeMallorca", "Bali", "HongKongSAR" ];
 let restaurants = ["Little Caesars", "Ruth's Chris Steak House", "McCormick & Schmick's", "American Deli", "Free birds World Burrito", "Cafe Rio Mexican Grill", "In-N-Out Burger", "The Cheese cake Factory", "Chick-fil-A", "Papa Murphy's" ];
@@ -13,40 +14,50 @@ let transportation = ["Rental Car", " Tour Bus", " Taxi", "City Bus", "Uber/Lift
 let entertainments = [" Cite Seeing", "Night Clubs", " Indor Games", "Casinos", "Water Activities"];
 
 
-
+// A function to randomly generate and suggest destination city
 function destinSelector( num){
 
     let selectd = destinations[num];
-    console.log(selectd);    
+    console.log(" Suggested Destination City :" + selectd);
+        
 return selectd;
 
 }
 
+
+// A function to randomly generate and suggest a restaurant
 function restaurantSelector( num2){
 
     let selectdRestaurant = restaurants[num2];
-    console.log(selectdRestaurant);    
+    console.log("Suggested restaurant :" + selectdRestaurant);
+        
 return selectdRestaurant;
 
 }
 
+ 
+// Afunction to randomly generate and suggest mode of transportation
 function transportSelector( num3){
 
     let selectdtransport = transportation[num3];
-    console.log(selectdtransport);    
+    console.log("Suggested Transportation Mode: " + selectdtransport);    
 return selectdtransport;
 
 }
 
+
+// Afunction to randomly generate and suggest entertainment
+
 function entertainmentSelector( num4){
 
     let selectdEntertainment = entertainments[num4];
-    console.log(selectdEntertainment);    
+    console.log("Suggested Entertainment: " + selectdEntertainment);    
 return selectdEntertainment;
 
 }
 
 
+// A function to generate random numbers
 function randNumGenerator(maxPlusOne) {
     let theRandNum;
     theRandNum = Math.floor(Math.random()  *  maxPlusOne);
@@ -54,11 +65,64 @@ function randNumGenerator(maxPlusOne) {
 }
 
 
-let generatIt  = randNumGenerator(20);
-let generatRestaurant = randNumGenerator(10);
-let generatTransport = randNumGenerator(5);
-let generatEntertainment = randNumGenerator(5);
-let ranSel = destinSelector(generatIt);
-let ranSelRestaurant = restaurantSelector(generatRestaurant);
-let ranSelTransport = transportSelector(generatTransport);
-let ranSelEntertainment = entertainmentSelector(generatEntertainment);
+
+//Menue for user
+console.log(" Enter 1 for destination City suggestions \n",
+              "Enter 2 for Restaurant Suggestions \n","Enter 3 for Transportation mode suggestions\n",
+              "Enter 4 for Entertainment suggestions \n", "Enter 5 for list of choosen options\n"
+              );
+
+
+let userInput = 0 ;
+let destinCityChoosen ;
+let restaurantChoosen;
+let transportChoosen;
+let entertainmentChoosen;
+
+ 
+// While loop to enable user randomly generate suggestions till satisfied
+    while (userInput != "5") {      
+        userInput = prompt( "Enter your choise here");     
+  
+
+    if (userInput == "1"){
+        let generatIt  = randNumGenerator(20);
+        let ranSel = destinSelector(generatIt);
+        destinCityChoosen = ranSel;
+        
+
+    }else if (userInput == "2"){
+        let generatRestaurant = randNumGenerator(10);
+        let ranSelRestaurant = restaurantSelector(generatRestaurant);
+        restaurantChoosen = ranSelRestaurant; 
+        
+    }else if (userInput == "3"){
+        let generatTransport = randNumGenerator(5);
+        let ranSelTransport = transportSelector(generatTransport);
+        transportChoosen = ranSelTransport;
+    }else if (userInput == "4"){
+        let generatEntertainment = randNumGenerator(5);
+        let ranSelEntertainment = entertainmentSelector(generatEntertainment);
+        entertainmentChoosen = ranSelEntertainment;
+    }else if (userInput == "5"){
+        console.log("Choosen Destination City :" + destinCityChoosen );
+        console.log("Choosen restaurant :" + restaurantChoosen);
+        console.log("Choosen Transportation Mode: " + transportChoosen);
+        console.log("Choosen Entertainment: " + entertainmentChoosen);
+    }
+   else{
+    console.log("invalid input")}
+};
+ 
+    
+
+   
+
+
+
+
+
+
+
+
+
