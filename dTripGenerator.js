9ef69b2b4b74d7cd855e1815a801a9315297ec9d
Aferu Minas <seyfeminas@gmail.com>
2021-08-01 17:23:50 -0700

    Project developed to randomly generate mode of transportation and entertainment options

--- dTripGenerator.js
+++ dTripGenerator.js
@@ -9,7 +9,8 @@
 
 let destinations = ["Bangkok", "Paris", "London", "Dubai", "Singapore", "KualaLumpur", "NewYork", "Istanbul", "Tokyo", "Antalya", "Seoul", "Osaka", "Makkah", "Phuket", "Pattaya", "Milan", "Barcelona", "PalmaDeMallorca", "Bali", "HongKongSAR" ];
 let restaurants = ["Little Caesars", "Ruth's Chris Steak House", "McCormick & Schmick's", "American Deli", "Free birds World Burrito", "Cafe Rio Mexican Grill", "In-N-Out Burger", "The Cheese cake Factory", "Chick-fil-A", "Papa Murphy's" ];
-
+let transportation = ["Rental Car", " Tour Bus", " Taxi", "City Bus", "Uber/Lift"];
+let entertainments = [" Cite Seeing", "Night Clubs", " Indor Games", "Casinos", "Water Activities"];
 
 
 
@@ -29,6 +30,22 @@ return selectdRestaurant;
 
 }
 
+function transportSelector( num3){
+
+    let selectdtransport = transportation[num3];
+    console.log(selectdtransport);    
+return selectdtransport;
+
+}
+
+function entertainmentSelector( num4){
+
+    let selectdEntertainment = entertainments[num4];
+    console.log(selectdEntertainment);    
+return selectdEntertainment;
+
+}
+
 
 function randNumGenerator(maxPlusOne) {
     let theRandNum;
@@ -37,7 +54,11 @@ function randNumGenerator(maxPlusOne) {
 }
 
 
-let generatIt  = randNumGenerator(21);
-let generatRestaurant = randNumGenerator(11);
+let generatIt  = randNumGenerator(20);
+let generatRestaurant = randNumGenerator(10);
+let generatTransport = randNumGenerator(5);
+let generatEntertainment = randNumGenerator(5);
 let ranSel = destinSelector(generatIt);
 let ranSelRestaurant = restaurantSelector(generatRestaurant);
+let ranSelTransport = transportSelector(generatTransport);
+let ranSelEntertainment = entertainmentSelector(generatEntertainment);
